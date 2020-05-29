93598155aa59f2ac0ea84f3651d745c4b51cf55a
Elijah Kirschen <ekirschen584@gmail.com>
2020-05-29 17:15:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,8 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
+  the_manifest = old_timey_things
+  old_timey_things = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
 
 end
 
